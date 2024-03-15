8f12686e340a4460271b0d9e5194704440eeafe7
Lotus Biswas <131962991+l0tus-biswas@users.noreply.github.com>
2024-03-16 00:11:50 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, 'dist/mtcrenovations')));
+app.use(express.static(path.join(__dirname, 'dist/candian-smart-savings-website')));
 
 // Catch all other routes and return the 'index.html' file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/mtcrenovations/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/candian-smart-savings-website/index.html'));
 });
 
 // Start the Express server
-const port = process.env.PORT || 3300;
+const port = process.env.PORT || 3700;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
